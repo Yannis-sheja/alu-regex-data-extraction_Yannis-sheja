a6330ce68ee5fd05b89de2a4b2cdc69885b98731
yann <y.sheja@alustudent.com>
2026-09-11 13:37:38 +0200

    Card Verification

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ INPUT = "input/raw-text.txt"
 with open(INPUT, "r") as file:
     text = file.read()
 
-email = r"^\b[\w._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"     #This is the regex pattern for the email
+email = r"^\b[\w._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"     #This is verify for the email
 #So, Email is: Username(letter, digits, underscore, dot,%,+ and -) + @ + Domain + Domain extension
 
 emails = re.findall(email, text)
@@ -34,17 +34,41 @@ for entry in emails:
         not_alu_emails.append(entry)
         print("OTHER EMAILS=", entry)
 
-phone_number = r"\+250\d{9}\b"  #This is the regex pattern for phone number
+phone_number = r"\+250\d{9}\b"  #This is verify for phone number
 #It mainly checks for Rwandan numbers only: +250 then any 9 digits
 
 phone_numbers = re.findall(phone_number, text)
 
-Pattern_24H = r"\b(?:[01]\d|2[0-3]):[0-5]\d(?::[0-5]\d)?\b(?!s?[APap][Mm])" #This is for time in 24h
+Pattern_24H = r"\b(?:[01]\d|2[0-3]):[0-5]\d(?::[0-5]\d)?\b(?!s?[APap][Mm])" #This is to verify for time in 24h
 # So, 00-23 hours, then 00-59 minutes and then seconds which is optional
 #Seconds are skipped whenever AM/PM follows
-Pattern_12H = r"\b(?:0?[1-9]1[0-2]):[0-5]\d\s?[APap][Mm]\b"         #This is for time 12h 
+Pattern_12H = r"\b(?:0?[1-9]1[0-2]):[0-5]\d\s?[APap][Mm]\b"         #This is verify for time 12h 
 #So, 1-12 hours, then 00-59 minutes and then the AM/PM
 
 time_in_24H = re.findall(Pattern_24H, text)
 time_in_12H = re.findall(Pattern_12H, text)
+
+credit_card = r"\b\d{4}[ -]?\d{4}[ -]?\d{4}\b"  #This is to verify for a credit card
+# Here we have four groups of four digits each, you can add space, dashes or no separator at all
+
+credit_cards = re.findall(credit_card, text)
+
+hidden_cards = []
+for number in credit_cards: 
+    digits = re.sub(r"[ -]","", number)     # This is to drop the seperators first so that the last -4 slice always lines up
+    hidden_cards.append("**** **** **** " + digits[-4:])    #Here every number is hidden except the last four digits
+
+risks = r"<script|<img|drop\s+table|delete\s+from|javascript:"
+risk_flag = bool(re.search(risks, text, flags = 2))
+
+final_report = {
+    "emails": {
+        "official":alu_official,
+        "alumni": alu_alumni,
+        "si": alu_si,
+        "others": not_alu_emails,
+        "hidden email": hidden_emails
+    },
+    "phone_numbers": phone_numbers 
+}
 
