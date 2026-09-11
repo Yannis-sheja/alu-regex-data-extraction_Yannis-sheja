6168717d42b5834b320e9d4d34a303c534f310ae
yann <y.sheja@alustudent.com>
2026-09-11 12:43:34 +0200

    Email Verification

--- src/main.py
+++ src/main.py
@@ -1,4 +1,36 @@
 import re 
 import json
 
-with open 
+INPUT = "input/raw-text.txt"
+
+with open(INPUT, "r") as file:
+    text = file.read()
+
+email = r"^\b[\w._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"     #This is the regex pattern for the email
+#So, Email is: Username(letter, digits, underscore, dot,%,+ and -) + @ + Domain + Domain extension
+
+emails = re.findall(email, text)
+
+alu_official = []
+alu_alumni = []
+alu_si = []
+not_alu_emails = []
+hidden_emails = []
+
+for entry in emails:
+    username, domain = entry.split("@")
+    hidden_emails.append(username[0]+ "***@" + domain)  # Only the first letter of the username will stay visible the rest will be hidden
+
+    if entry.endswith("@alumni.alueducation.com"):  # This must be checked before the official @alueducation.com
+        alu_alumni.append(entry)                    #This is mainly because they all end in the same text
+        print("ALU ALUMNI=", entry)
+    elif entry.endswith("@si.alueducation.com"):    #The same reason for the SI domain
+        alu_si.append(entry)
+        print("ALU SI=", entry)
+    elif entry.endswith("@alueducation.com"):
+        alu_official.append(entry)
+        print("ALU OFFICIAL=", entry)
+    else:
+        not_alu_emails.append(entry)
+        print("OTHER EMAILS=", entry)
+        
